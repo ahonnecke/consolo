dfe2c05cf901f58d092bfc87ed00e79a2c0c4f70
Ashton Honnecke <ashton@pixelstub.com>
2023-02-21 13:02:06 -0700

    Update execuable path.

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # This installation script assumes that ~/bin/ is already in your path
 
-CONSOLO="https://raw.githubusercontent.com/ahonnecke/consolo/e15cb77e534a3d37c2d78b0001f414e38078e738/consolo.py"
+CONSOLO="https://raw.githubusercontent.com/ahonnecke/consolo/main/consolo.py"
 
-
-pip install argdantic boto3 requests watchdog zipfile
-curl $CONSOLO --output ~/bin/consolo
-
-chmod +x ~/bin/consolo
+pip install argdantic boto3 requests watchdog zipfile && \
+curl $CONSOLO --output ~/bin/consolo && \
+chmod +x ~/bin/consolo &&
